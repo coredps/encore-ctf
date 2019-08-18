c078e62a2f6feadeacd4de9566ddee5ee16ccd1b
Tarush Sonakya <tsonakya@gmail.com>
2019-08-18 19:20:45 +0530

    Format string vuln

---
+++ Format me/format(solution).c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char *str;
+    char *secret="enc0re{f0rm4t_5tr1n6_vuln3r4b1l1ty}";
+    puts("I am hungry for the flag! NOm nOM");
+    scanf("%s",str);
+    printf(str);
+    puts("\nthat's so sad ByE");
+    return 0;
+}
