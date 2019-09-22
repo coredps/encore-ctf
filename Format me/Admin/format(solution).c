7aba5c06bd34d95e38a894789c3cea60fcab24ee
Tarush Sonakya <tsonakya@gmail.com>
2019-09-22 14:46:24 +0000

    Update format(solution).c

--- Format me/Admin/format(solution).c
+++ Format me/Admin/format(solution).c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
-int main()
-{
+int main(int argc, char **argv[]) {
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
+
     char *str;   
-    char *secret="enc0re{-This_is_the_address_to_flag}";
+    char *secret="enc0re{f0rm4t_5tr1n6_vuln3r4b1l1ty}";
+    printf("Please input value\n");
     scanf("%s",str);
     puts("aight imma just printf it again");
     printf(str);
-    puts("\nexiting xD");
     exit(1);
+    return 0;
 }
