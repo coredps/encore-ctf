81ad9291c02e1b1f8bc6639e720d347933e66616
TarushS <tsonakya@gmail.com>
2020-07-24 05:09:03 +0530

    added upload vuln challenge

---
+++ internal/README.md
@@ -0,0 +1,15 @@
+# EXT
+dis sum shady website. Don't try to be a leet heccur ;)
+<pre>
+Report to admins if the flag is missing(isn't that obvious) or you are unable to get a connection back.
+</pre>
+
+## Flag
+```
+enc0re{f1ile_Upl04d_vuLns_ar3_sh3xy}
+```
+
+## Solution
+1. Upload the .phtml file to the webserver and access it
+2. you will see tons of files
+3. find flag using grep or just use ls, cd, cat xD
---
+++ internal/admin/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<style>
+html, body {
+    height: 30%;
+}
+html {
+    display: table;
+    margin: auto;
+}
+body {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+}
+</style>
+</head>
+<body>
+<form action="index.php" method="post" enctype="multipart/form-data">
+    <h3>Upload ;)</h3><br />
+    <input type="file" name="file" id="file">
+    <input class="btn btn-primary" type="submit" value="Submit" name="submit">
+</form>
+<?php
+   if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
+      
+      $extensions= array("phtml");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors="tryna be a heccur huh?";
+      }
+            
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"uploads/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+</body>
+</html>
