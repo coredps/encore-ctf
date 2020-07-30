dcc306624274306286dbb5141cd535912f0c08a0
TarushS <tsonakya@gmail.com>
2020-07-30 13:54:18 +0530

    dockerignore for Dockerfile and Makefile

--- create_your_browser/admin/index.php
+++ create_your_browser/admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
-define('MY_USER_AGENT', 'Samsung smart television fridge');
-define('REDIRECT_LOCATION', 'https://www.wikihow.com/Make-a-Web-Browser');
+define('MY_USER_AGENT', 'Samsung smart fridge');
+define('REDIRECT_LOCATION', 'https://mcow.ml/');
 
 if ($_SERVER['HTTP_USER_AGENT'] !== MY_USER_AGENT) {
     header('Location: ' . REDIRECT_LOCATION);
@@ -14,5 +14,6 @@ if ($_SERVER['HTTP_USER_AGENT'] !== MY_USER_AGENT) {
     <body>
         This website is only accessible using <br>
         Samsung smart television fridge
+        enc0re{mc0w_mL_dD05inG}
     </body>
 <html>
