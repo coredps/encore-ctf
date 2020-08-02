0807487eb5945a44cd7b580a1296af97283f8f97
TarushS <tsonakya@gmail.com>
2020-08-02 08:44:25 +0530

    fixed some challs

--- create_your_browser/admin/index.php
+++ create_your_browser/admin/index.php
@@ -9,11 +9,5 @@ if ($_SERVER['HTTP_USER_AGENT'] !== MY_USER_AGENT) {
 }
 ?>
 
-<html>
-<title>Create a browser</title>
-    <body>
-        This website is only accessible using <br>
-        Samsung smart television fridge
+
         enc0re{mc0w_mL_dD05inG}
-    </body>
-<html>
